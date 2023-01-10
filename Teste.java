0429dc635220ae324d67594fc9db1f93aee46386
Jotapppe <117678443+Jotapppe@users.noreply.github.com>
2023-01-09 22:31:49 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,16 @@
+package Exercicio9;
+
+public class Teste {
+	public Cliente1 extends Teste;
+	public static void main( String args[])
+	{
+	//** TODOS OS OBJETOS OU "PESSOAS"**/
+	
+		Cliente1 cliente1 = new Cliente1();
+				cliente1.Primeiro ="Gustavo";
+				cliente1.Sobre ="Henrique";
+				cliente1.Endereço="Rua Professor Barroso do amaral, 529";
+			System.out.println(cliente1.getCliente1());	
+				//**ESTANCIAMENTO*//							  	//**PARAMETROS*/
+	}
+}
