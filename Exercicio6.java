8b64b3771beca8673896a23ababd1e31844f7ed4
Jotapppe <117678443+Jotapppe@users.noreply.github.com>
2023-01-09 21:59:32 -0300

    Add files via upload

--- Exercicio6.java
+++ Exercicio6.java
@@ -22,6 +22,29 @@ public class Exercicio6 {
 
 
 	}
-	
+	package Exercicio5;
+
+	import java.util.Scanner;
+
+	public class Exercicio5 {
+		
+		public static void main(String[] args) {
+		       
+	int n, s= 0;
+	String resp;
+	Scanner leia = new Scanner (System.in);
+	/**VARIAVEL**/
+
+		do {
+			System.out.print("Digite um numero: ");
+			n = leia.nextInt();
+			s += n;
+			System.out.print("Voce quer continuar? [S/N] ");
+			resp = leia.next();
+		} while (resp.equals("s"));
+		System.out.print("A soma dos valores é:" +s);
+		
+	/**LAÇO**/
+		
 
 }
