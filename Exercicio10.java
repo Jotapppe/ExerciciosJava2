f03513572dbaae9be9fbdc42a5efe8fcaf023e1e
Jotapppe <117678443+Jotapppe@users.noreply.github.com>
2023-01-09 22:27:59 -0300

    Add files via upload

---
+++ Exercicio10.java
@@ -0,0 +1,13 @@
+package Exercicio10;
+public class Exercicio10 {
+
+	public static void main(String[] args) {
+		int n[]= {9,7,8,6,5,3,2,1};
+		
+		
+		for (int c=0 ; c<=8; c++) {
+			System.out.print(n[c] + " ");
+
+	}
+}
+}
